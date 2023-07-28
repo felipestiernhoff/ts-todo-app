b93407b2c9cd1109047aa547b820f6043e357cef
felipestiernhoff <felipehipsman@hotmail.com>
2023-07-28 17:50:45 +0200

    Integrate useTodo hook to manage todo items.

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,29 +1,39 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-hot-toast'
-import { useTodo } from '../context'
+import { useTodo } from '../context/useTodo'
 import { Input } from './Input'
 
 export const AddTodo = () => {
   const [input, setInput] = useState<string>('')
-  const [todos, setTodos] = useState<string[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+  const { addTodo } = useTodo()
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
 
   const handleSubmission = (e: React.FormEvent) => {
     e.preventDefault()
     if (input.trim() !== '') {
-      setTodos([...todos, input])
+      addTodo(input)
       setInput('')
+      toast.success('Todo added successfully!')
+    } else {
+      toast.error('Todo field cannot be empty!')
     }
   }
 
   return (
     <form onSubmit={handleSubmission}>
       <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
-        <input
+        <Input
+          ref={inputRef}
+          type="text"
+          placeholder="start typing ..."
           value={input}
           onChange={e => setInput(e.target.value)}
-          type="text"
-          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
-          placeholder="start typing ..."
         />
         <button
           type="submit"
